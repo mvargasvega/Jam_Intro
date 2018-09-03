5380b00f2c7cdd7a0eeb0a8bb7408091e9769482
mvargasvega <martinvargasvega@gmail.com>
2018-09-02 19:53:59 -0700

    Modified first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+
     <h1> Hello my name is Martin Vargas-Vega.</h1>
     <h3>Bio</h3>
-    <p> I was born and raised in Watsonville,CA and my goal is to be a fulltime software engineer.</p>
+    <p> I was born and raised in Watsonville,CA and my goal is to be a fulltime software engineer.
+    I am excited to be part of this event and code with other developers.
+    </p>
+
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
