865e3716e342fcf0524de3281091c07363c8d99d
mvargasvega <martinvargasvega@gmail.com>
2018-09-02 20:07:36 -0700

    Added experience & prior hackathons

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,27 @@ const IndexPage = () => (
     I am excited to be part of this event and code with other developers.
     </p>
 
+    <h3> Coding Experience</h3>
+    <ul>
+      <li>React</li>
+      <li>MEN Stack</li>
+      <li>Ruby on Rails</li>
+    </ul>
+
+    <h3> UX/UI Tools</h3>
+    <ul>
+      <li>Adobe XD</li>
+      <li>Sketch</li>
+    </ul>
+
+    <h3> Prior Hackathons</h3>
+    <ul>
+      <li>Oakland Startup Weekend August 2018</li>
+      <li>#Stayhacking @ Netflix August 2018</li>
+      <li>DREAMer Hack April 2018</li>
+      <li>ScriptEd Developer Mentor</li>
+    </ul>
+
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
